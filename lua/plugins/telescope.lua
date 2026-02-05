20db0fa9550824a0be9f4f98fe96e971549f0d33
Marco GILLES <marco.gilles@virbac.com>
2026-02-05 22:00:52 +0100

    status line, indentation mark, live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     keys = {
-	{ "<leader>p", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", desc = "Find files" },
-	{ "<leader>l", "<cmd>Telescope live_grep hidden=true no_ignore=true<cr>", desc = "Find string occurence" },
+        { "<leader>p", "<cmd>Telescope find_files hidden=true no_ignore=false<cr>", desc = "Find files" },
+        { "<leader>l", "<cmd>Telescope live_grep hidden=true no_ignore=false<cr>",  desc = "Find string occurence" },
     }
 }
